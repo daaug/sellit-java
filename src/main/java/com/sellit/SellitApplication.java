e1756dbd53d3192ed0d2a372cdfc2542e1a224bf
Daniel Augusto <daniel.aug.souza@gmail.com>
2025-11-28 23:02:51 -0300

    First commit

---
+++ src/main/java/com/sellit/SellitApplication.java
@@ -0,0 +1,13 @@
+package com.sellit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellitApplication.class, args);
+	}
+
+}
